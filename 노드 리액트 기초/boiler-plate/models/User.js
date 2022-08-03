548155666597cdc05128608ea7adfae418469118
eunbi lee <led156@naver.com>
2022-08-03 19:20:31 +0900

    Bcrypt로 비밀번호 암호화

--- 노드 리액트 기초/boiler-plate/models/User.js
+++ 노드 리액트 기초/boiler-plate/models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-
-
+const bcrypt = require('bcrypt');
+const saltRounds = 10; // salt를 이용해 비밀번호를 암호화. (saltRounds = 몇글자인지)
 
 const userSchema = mongoose.Schema({
     name: {
@@ -32,6 +32,23 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 })
+
+userSchema.pre('save', function( next ) {
+    var user = this;
+
+    if (user.isModified('password')) {  // 패스워드를 바꿀 때만 해당 함수 실행 (이메일 등등 해당X)
+        // 비밀번호 암호화
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            if(err) return next(err)
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if(err) return next(err)
+                user.password = hash
+                next()
+            })
+        })
+    }
+    
+}) // 몽구스를 이용. save 전에 해당 함수를 실행. 끝난 후 next로 돌아감.
 
 const User = mongoose.model('User', userSchema)
 
