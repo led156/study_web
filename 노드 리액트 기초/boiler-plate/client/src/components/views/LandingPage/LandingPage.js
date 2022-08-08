bc54006e4624c34d6f14da848ffac95e071f730f
eunbi lee <led156@naver.com>
2022-08-08 19:45:58 +0900

    노드 리액트 기초 : logout button

--- 노드 리액트 기초/boiler-plate/client/src/components/views/LandingPage/LandingPage.js
+++ 노드 리액트 기초/boiler-plate/client/src/components/views/LandingPage/LandingPage.js
@@ -1,11 +1,26 @@
 import React, { useEffect } from 'react'
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom'
+// import { response } from 'express';
 
 function LandingPage() {
+  let navigate = useNavigate()
+
   useEffect(() => {
     axios.get('/api/hello')
     .then(response => console.log(response.data))
   }, [])
+
+  const onClickHandler = () => {
+    axios.get('/api/users/logout')
+    .then(response => {
+      if (response.data.success) {
+        navigate('/login')
+      } else {
+        alert('로그아웃을 다시 시도해주십시오.')
+      }
+    })
+  }
 
   return (
     <div style={{
@@ -13,6 +28,10 @@ function LandingPage() {
       , width: '100%', height: '100vh'
   }}>
       <h2>시작 페이지</h2>
+
+      <button onClick={onClickHandler}>
+          로그아웃
+      </button>
   </div>
   )
 }
