846fac06a8402a2b4e829497e8890a015223d858
eunbi lee <led156@naver.com>
2022-08-12 17:58:19 +0900

    스프링 입문 : H2 DB, JDBC

--- 스프링 입문 스프링부트/hello-spring/src/main/java/com/led/hellospring/SpringConfig.java
+++ 스프링 입문 스프링부트/hello-spring/src/main/java/com/led/hellospring/SpringConfig.java
@@ -1,13 +1,24 @@
 package com.led.hellospring;
 
+import com.led.hellospring.repository.JdbcMemberRepository;
 import com.led.hellospring.repository.MemberRepository;
 import com.led.hellospring.repository.MemoryMemberRepository;
 import com.led.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean // 스프링 빈에 등록한다.
     public MemberService memberService() {
@@ -16,6 +27,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        // return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
